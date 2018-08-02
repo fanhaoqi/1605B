beb1cbb8a8e49e89985b414210d864906458b4eb
fanhaoqi <2441435915@qq.com>
2018-08-02 09:35:18 +0800

    淘宝首页

---
+++ tao/js/script.js
@@ -0,0 +1,19 @@
+// new Swiper('.banner', {
+//     autoplay: {
+//         delay: 2000
+//     },
+//     loop: true,
+//     pagination: {
+//         el: '.page',
+//         clickable: true
+//     }
+// });new Swiper('.banner', {
+//     autoplay: {
+//         delay: 2000
+//     },
+//     loop: true,
+//     pagination: {
+//         el: '.page',
+//         clickable: true
+//     }
+// });
